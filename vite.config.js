307dc8824836289c2053493033c13da1b8e41e88
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-08-03 21:18:35 +0400

    Updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/eventfulogee4.github.io/',
+  base: '/portfolio-website2.0/',
   plugins: [react()],
 })
