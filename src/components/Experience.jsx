29d0061506b496d6abb9b3399f56648735a44c65
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-09-18 01:48:09 -0600

    Fixed the user responsiveness issue

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,12 +16,12 @@ export default function Experience(props){
                 </div>
                 <div className="experience-summary">
                     <p>
-                    During my time as a Software Developer at Kapital Bank, I had the opportunity to work on real-world projects that contributed directly to the bank’s 
-                    digital infrastructure. I collaborated with a team of developers to design, implement, and maintain internal systems and customer-facing applications. 
-                    My role involved writing clean, efficient code, debugging issues, and ensuring the reliability and performance of our software solutions. 
-                    I gained hands-on experience with tools and technologies such as HTML, CSS, JAVASCRIPT, and ReactJS 
-                    and strengthened my skills in agile development, teamwork, and problem-solving in a fast-paced environment.
-                </p>
+                        During my time as a Software Developer at Kapital Bank, I had the opportunity to work on real-world projects that contributed directly to the bank’s 
+                        digital infrastructure. I collaborated with a team of developers to design, implement, and maintain internal systems and customer-facing applications. 
+                        My role involved writing clean, efficient code, debugging issues, and ensuring the reliability and performance of our software solutions. 
+                        I gained hands-on experience with tools and technologies such as HTML, CSS, JAVASCRIPT, and ReactJS 
+                        and strengthened my skills in agile development, teamwork, and problem-solving in a fast-paced environment.
+                    </p>
                 </div>
         </section>
     )
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,13 +4,13 @@ export default function Intro(props){
 
     return (
         <section ref={props.introSection} class="intro-section">
-            <div class="animate__animated animate__backInDown">
-                <div class="name-container">
+            <div className="animate__animated animate__backInDown">
+                <div className="name-container">
                     <h1>
                         Javad Mammadaliyev
                     </h1>
                 </div>
-                <div class="info-container">
+                <div className="info-container">
                     <h2>Coding with purpose and passion.</h2>
                 </div>
             </div>
