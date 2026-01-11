fdac1b7610229ad5de58e4cc6c88fba0ff9052fb
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2026-01-11 20:20:45 +0400

    Made some updates for the Experience ].jsx file

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,13 +15,13 @@ export default function Experience(props){
                     <h3>Software Developer Intern</h3>
                 </div>
                 <div className="experience-summary">
-                    <p>
+                    <h3>
                         During my time as a Software Developer at Kapital Bank, I had the opportunity to work on real-world projects that contributed directly to the bank’s 
                         digital infrastructure. I collaborated with a team of developers to design, implement, and maintain internal systems and customer-facing applications. 
                         My role involved writing clean, efficient code, debugging issues, and ensuring the reliability and performance of our software solutions. 
                         I gained hands-on experience with tools and technologies such as HTML, CSS, JAVASCRIPT, and ReactJS 
                         and strengthened my skills in agile development, teamwork, and problem-solving in a fast-paced environment.
-                    </p>
+                    </h3>
                 </div>
         </section>
     )
